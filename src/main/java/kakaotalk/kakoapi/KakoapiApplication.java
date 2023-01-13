c52537641c76a4bb4dd579428bef9ccfeeab76b4
kosmos95 <86996613+kosmos95@users.noreply.github.com>
2023-01-13 23:20:25 +0900

    Springboot GENERATE
    START

---
+++ src/main/java/kakaotalk/kakoapi/KakoapiApplication.java
@@ -0,0 +1,13 @@
+package kakaotalk.kakoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kakaotalk/kakoapi/KakoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package kakaotalk.kakoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
